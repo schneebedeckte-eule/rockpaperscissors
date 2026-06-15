084266d924730bf391f394c8cc9f92f49e6801cb
Joshua <246270231+schneebedeckte-eule@users.noreply.github.com>
2026-06-15 14:45:26 +0200

    create getHumanChoice Function

--- rps.js
+++ rps.js
@@ -1,6 +1,7 @@
 console.log("hello");
 
 let computerChoice;
+let humanChoice;
 
 // pseudocode step 1: create a function that randomly returns either rock paper or scissors
 // create a random number between 0 and 1
@@ -19,4 +20,24 @@ function getComputerChoice() {
     computerChoice = "Scissors";
   }
   return computerChoice;
+}
+
+// pseudocode step2: create a function that lets the human player input the options
+// prompt the human to input a choice
+// make the user input case insensitive
+// return error if user types invalid Option
+
+function getHumanChoice() {
+  let userInput = prompt("Please choose either Rock, Paper or Scissors.");
+  let userInputLowerCase = userInput.toLowerCase();
+  if (userInputLowerCase === "rock") {
+    humanChoice = "Rock";
+  } else if (userInputLowerCase === "paper") {
+    humanChoice = "Paper";
+  } else if (userInputLowerCase === "scissors") {
+    humanChoice = "Scissors";
+  } else {
+    humanChoice = "You entered an invalid Option.";
+  }
+  return humanChoice;
 }
