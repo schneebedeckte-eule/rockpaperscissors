e9f8e0d0d65bebcead32a06776f463c44b3c3e53
Joshua <246270231+schneebedeckte-eule@users.noreply.github.com>
2026-06-16 19:08:20 +0200

    create getComChoice function

--- rps2.js
+++ rps2.js
@@ -1,1 +1,15 @@
 // trying to recreate the whole game without tips or hints, one day after first guided attempt
+
+let humanScore = 0;
+let comScore = 0;
+
+function getComChoice() {
+  let randomNum = Math.random();
+  if (randomNum <= 0.33) {
+    return "Rock";
+  } else if (randomNum <= 0.66) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
