78cd3b984e86c2e10d1ef5b2244fcba85d89db70
Joshua <246270231+schneebedeckte-eule@users.noreply.github.com>
2026-06-16 19:13:14 +0200

    create getHumanChoice function

--- rps2.js
+++ rps2.js
@@ -13,3 +13,17 @@ function getComChoice() {
     return "Scissors";
   }
 }
+
+function getHumanChoice() {
+  let humanInput = prompt("Rock, Paper or Scissors?");
+  let humanLower = humanInput.toLowerCase();
+  if (humanLower === "rock") {
+    return "Rock";
+  } else if (humanLower === "paper") {
+    return "Paper";
+  } else if (humanLower === "scissors") {
+    return "Scissors";
+  } else {
+    return "You entered an invalid Choice.";
+  }
+}
