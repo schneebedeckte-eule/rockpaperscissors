9bd4c220cb331dceae23ab17fd51f9107c2c4084
Joshua <246270231+schneebedeckte-eule@users.noreply.github.com>
2026-06-17 14:08:41 +0200

    create getComChoice function

--- rps3.js
+++ rps3.js
@@ -1,1 +1,15 @@
 // third attempt yet again a day later, this time without mistakes or double-checking
+
+humanScore = 0;
+comScore = 0;
+
+function getComChoice() {
+  let randomNum = Math.random();
+  if (randomNum <= 0.33) {
+    return "Rock";
+  } else if (randomNum <= 0.66) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
